2b62313514d2d8c3fb3dc079377c041083a4d26f
Cl0n3 <108764367+Cl0n3Tr0op3r@users.noreply.github.com>
2024-10-04 17:55:52 -0500

    Fixed scripting for 2d character movement
    LESSS GOOOO

--- Assets/PlayerControl.cs
+++ Assets/PlayerControl.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerControl : MonoBehaviour
 {
@@ -15,33 +16,36 @@ public class PlayerControl : MonoBehaviour
     void Start()
     {
         rb = gameObject.GetComponent<Rigidbody>();
+        sr = gameObject.GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Raycast to check if the player is grounded and adjust position
         RaycastHit hit;
         Vector3 castPos = transform.position;
-        castPos.y +=1;
-        if(Physics.Raycast(castPos, -transform.up, out hit, Mathf.Infinity, terrainLayer))
+        castPos.y += 1;
+        if (Physics.Raycast(castPos, -transform.up, out hit, Mathf.Infinity, terrainLayer))
         {
             Vector3 movePos = transform.position;
             movePos.y = hit.point.y + groundDist;
             transform.position = movePos;
         }
-    }
 
-    float x = Input.GetAxis("Horizontal");
-    float y = Input.GetAxis("Vertical");
-    Vector3 moveDir = new Vector3(x, 0, y);
-    rb.velocity = moveDir * speed
+        // Movement and sprite flipping
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+        Vector3 moveDir = new Vector3(x, 0, y);
+        rb.velocity = moveDir * speed;
 
-    if(x!=0 && x<0)
-    {
-        sr.flipX = true;
-    }
-    else if(x!=0 && x>0)
-    {
-        sr.flipX = false;
+        if (x != 0 && x < 0)
+        {
+            sr.flipX = true;
+        }
+        else if (x != 0 && x > 0)
+        {
+            sr.flipX = false;
+        }
     }
 }
