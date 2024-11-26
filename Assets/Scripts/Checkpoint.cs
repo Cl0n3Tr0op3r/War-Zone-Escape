16051d40b05f0175013c2f43f184d9a97afcc259
Cl0n3 <108764367+Cl0n3Tr0op3r@users.noreply.github.com>
2024-11-26 03:09:26 -0600

    moved barrier

--- Assets/Scripts/Checkpoint.cs
+++ Assets/Scripts/Checkpoint.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 
 public class Checkpoint : MonoBehaviour
 {
-   [SerializeField] GameObject player;
-   [SerializeField] List<GameObject> checkPoints;
-   [SerializeField] Vector3 vectorPoint;
-   [SerializeField] float dead;
+    [SerializeField] GameObject player;
+    [SerializeField] List<GameObject> checkPoints;
+    [SerializeField] Vector3 vectorPoint;
+    [SerializeField] float dead;
 
     // Update is called once per frame
     void Update()
@@ -21,6 +21,12 @@ public class Checkpoint : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         vectorPoint = player.transform.position;
-        Destroy(other.gameObject);
+
+        // Hide the object instead of destroying it
+        other.gameObject.SetActive(false); // Deactivates the object completely
+        
+        // Alternatively, disable specific components (like Renderer and Collider)
+        // other.GetComponent<Renderer>().enabled = false; // Hides the object visually
+        // other.GetComponent<Collider>().enabled = false; // Disables interaction
     }
 }
