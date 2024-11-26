9a96cc6d2ce06d1c69656d7420e843125d47f769
Cl0n3 <108764367+Cl0n3Tr0op3r@users.noreply.github.com>
2024-11-26 00:06:23 -0600

    Resolved merge issues

--- Assets/Scripts/Checkpoint.cs
+++ Assets/Scripts/Checkpoint.cs
@@ -2,17 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Checkpoints : MonoBehaviour
+public class Checkpoint : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+   [SerializeField] GameObject player;
+   [SerializeField] List<GameObject> checkPoints;
+   [SerializeField] Vector3 vectorPoint;
+   [SerializeField] float dead;
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (player.transform.position.y < dead)
+        {
+            player.transform.position = vectorPoint;
+        }
+    }
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        vectorPoint = player.transform.position;
+        Destroy(other.gameObject);
     }
 }
